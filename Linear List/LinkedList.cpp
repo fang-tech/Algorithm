aa468006255f43f40b954c6af4fb54517d29ad49
fang-tech <2819187982@qq.com>
2024-07-31 22:53:00 +0800

    finish LinkedList

--- Linear List/LinkedList.cpp
+++ Linear List/LinkedList.cpp
@@ -19,14 +19,14 @@ private:
     Node* head = nullptr;
     // 尾结点并不是一个特殊的结点, 只是用来标记最后一个结点便于一些操作
     Node* tail = nullptr;
-    int length;
+    int len;
 
 public: 
     // 构造函数
     LinkedList(){
         this->head = new Node();  
         this->tail = head;
-        this->length = 0;
+        this->len = 0;
     }
     // 析构函数, 用于释放内存
     // ~LinkedList(){
@@ -40,41 +40,48 @@ public:
     // 迭代器类的定义, 用于遍历链表, 迭代器是对于指针的再一次封装, 
     // 为外部提供一个统一的访问接口, 在C++的STL库中, 是为了容器和STL算法的兼容
     
-public: // 操作集 -> 基础操作集
-    Node* begin(){
+public: // 操作集 -> base operation
+    Node* begin() const {
         return head;
     }  
     // 返回head结点地址
-    Node* end(){
+    Node* end() const {
         return tail;
     };
-    bool Empty(){
+    bool isEmpty() const {
         if(head->next == nullptr)
             return true;
         else 
             return false;
     }
+    int length() const {
+        return len;
+    }
     // 返回tail结点地址
 
     // 删除线性表中的所有元素
-    void Clear();  
-    // 插入元素, index为int的时候, 在序号为idx的后面插入, 返回插入的结点的地址, 插入失败, 返回end()
-    void ListInsert(const T& elem, int idx);  
-    // 插入元素, index为某个结点迭代器, 在该结点的后面插入, 返回插入的结点的地址, 插入失败, 返回end()
-    void ListInsert(const T& elem, Node* idx);
+    void clear();  
+    // 插入元素, index为int的时候, 在序号为idx的前面插入, 返回插入的结点的地址, 插入失败, 返回False
+    bool insert(const T& elem, int idx);  
     // 尾插入元素
     void push_back(const T& elem);
     // 头插入元素
     void push_front(const T& elem);
+    // 在链表中查找元素, 如果元素存在, 则返回idx, 如果不存在则返回-1, 这个只能返回第一个查找到的第一个结点的位置
+    int find(const T& elem);
+    // 删除指定位置的元素
+    bool del(int idx);
+    // 获取指定位置的元素的值
+    T& at(int idx);
     // 打印所有元素
     void print_list(){
         Node* p = head;
         Node* q = p->next;
-        if (Empty())
+        if (isEmpty())
             cout << "链表为空" << endl;
         else
             cout << "链表不为空" << endl;
-        cout << "链表长度为 : " << length << endl;
+        cout << "链表长度为 : " << len << endl;
         cout << "链表内容 : "; 
         while(q){
             p = q->next;
@@ -83,9 +90,16 @@ public: // 操作集 -> 基础操作集
         }
         cout << endl;
     }
+public: // 操作集 -> advanced operation
+    // 合并两个有序链表
+    LinkedList<T> merge(const LinkedList<T>& other);
+    // 反转链表
+    void reverse();
+    // 判断链表是不是环状的
+    bool hasCircle();
 };
 
-template<class T> void LinkedList<T>::Clear(){
+template<class T> void LinkedList<T>::clear(){
 /*
     删除所有Node, 不包括头结点, 尾结点
     两种方式 :
@@ -112,7 +126,7 @@ template<class T> void LinkedList<T>::Clear(){
 
     head->next = nullptr;
     tail = head;
-    length = 0;
+    len = 0;
 }
 
 template<class T> void LinkedList<T>::push_back(const T& elem){
@@ -123,7 +137,7 @@ template<class T> void LinkedList<T>::push_back(const T& elem){
     tail->next = newNode;
     newNode->prev = tail;
     tail = newNode;
-    length++;
+    len++;
 }
 
 template<class T> void LinkedList<T>::push_front(const T& elem){
@@ -140,7 +154,191 @@ template<class T> void LinkedList<T>::push_front(const T& elem){
         newNode->next->prev = newNode;
     else
         tail = newNode;
-    length++;
+    len++;
+}
+
+template<class T> bool LinkedList<T>::insert(const T& elem, int idx){
+/*
+    根据idx, 插入元素, 会将元素插入到idx位置的前面
+*/
+    // 给出idx超出范围
+    if (idx < 0 || idx > len){
+        // for test
+        // {cout << "idx=" << idx << " 越界" << ", 当前len为" << len << endl;
+        // cout << "插入失败" << endl;}
+        return false;
+    }
+    // 找到idx位置的前一个结点
+    Node* p = head;
+    for (int i = 0; i < idx; i++, p=p->next){}
+    // 如果该结点是最后一个结点
+    if (p == tail)
+        this->push_back(elem);
+    // 如果是第一个结点
+    else if (p == head){
+        this->push_front(elem);
+    }
+    else {
+        Node* newNode = new Node(elem);
+        Node* q = p->next;
+        p->next = newNode;
+        newNode->next = q;
+        q->prev = newNode;
+        newNode->prev = p;
+        len++;
+    }
+    // for test
+    // {cout << elem <<"插入成功" << endl;}
+    return true;
+}
+
+template<class T> bool LinkedList<T>::del(int idx){
+/*
+    根据idx, 删除元素, 处理越界
+*/
+    // 给出idx超出范围
+    if (idx < 0 || idx >= len){
+        // for test
+        {cout << "idx=" << idx << " 越界" << ", 当前len为" << len << endl;
+        cout << "删除失败" << endl;}
+        return false;
+    }
+    // 链表为空
+    if (this->isEmpty())
+        return false;
+    // 找到idx位置的结点
+    Node* p = head;
+    for (int i = 0; i <= idx; i++, p=p->next){}
+    // 如果该结点是最后一个结点
+    if (p == tail){
+        Node* q = p->prev;
+        delete p;
+        q->next = nullptr;
+        tail = q;
+    }
+    // 如果是第一个结点
+    else if (p == head->next){
+        head->next = p->next;
+        delete p;
+    }
+    else {
+        Node* q = p->prev;
+        q->next = p->next;
+        delete p;
+        q->next->prev = q;
+    }
+    len--;
+    // for test
+    {this->print_list();}
+    return true;
+}
+
+template<class T> int LinkedList<T>::find(const T& elem){
+/*
+    根据结点的值查找结点的索引
+*/
+    int idx = -1;
+    Node* p = head->next;
+    for (int i = 0; p; i++, p = p->next){
+        if (p->data == elem){
+            idx = i;
+            break;
+        }
+    }
+    return idx;
+}
+
+template<class T> T& LinkedList<T>::at(int idx){
+/*
+    根据结点的索引查找结点的数据, 该函数处理越界采取了异常处理模式
+*/
+    if (idx < 0 || idx >= len){
+        // for test
+        {cout << "idx=" << idx << " 越界" << ", 当前len为" << len << endl;
+        cout << "查找失败" << endl;}
+        throw std::out_of_range("Index out of range");
+    }
+    Node* p = head;
+    for (int i = 0; i <= idx; i++, p=p->next){}
+    return p->data;
+}
+
+template<class T> LinkedList<T> LinkedList<T>::merge(const LinkedList<T>& other){
+/*
+    将两个有序链表合并为一个链表, 时间复杂度 : O(m+n)
+*/
+    LinkedList<T> list;
+    Node* p = this->head->next;
+    Node* q = other.begin()->next;
+    // 两个链表都没有被比较完的时候
+    while (p && q){
+        if (p->data >= q->data){
+            list.push_back(q->data);
+            q = q->next;
+        } else {
+            list.push_back(p->data);
+            p = p->next;
+        }
+    }
+
+    // 处理剩下的结点
+    while (p){
+        list.push_back(p->data);
+        p = p->next;
+    }
+
+    while (q){
+        list.push_back(q->data);
+        q = q->next;
+    }
+
+    return list;
+}
+
+template<class T> void LinkedList<T>::reverse(){
+/*
+    反转链表, 使用效率最高的迭代法, 在这里, 假设链表的实现是单向的
+    因为在双向链表上这个操作的实现很简单
+*/
+    // 空链表或者只有一个结点
+    if (isEmpty() || len == 1)
+        return;
+    Node* prev = nullptr;
+    Node* current = head->next;
+    Node* next = current->next;
+
+    tail = current;
+
+    while (current){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    head->next = prev;
+}
+
+template<class T> bool LinkedList<T>::hasCircle(){
+/*
+    用于检测链表中是否出现了环, 使用快慢指针法
+*/
+    // 处理空链表的情况 
+    if (head->next) return false;
+
+    Node* fast = head->next;
+    Node* slow = head->next;
+
+    while(fast && fast->next){
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void unit_test_base_operation(){
@@ -151,13 +349,68 @@ void unit_test_base_operation(){
     list.push_front(3);
     list.print_list();
     cout << "*******测试删除*******" <<endl;
-    list.Clear();
+    list.clear();
     list.print_list();
+    cout << "*******测试插入与查找*******" << endl;
+    list.push_front(1);
+    list.push_back(4);
+    list.push_front(3);
+    list.insert(5, 4);
+    list.insert(5, 3);
+    list.insert(19, 0);
+    list.print_list();
+    cout << "元素19的idx为" << list.find(19) << endl;
+    cout << "元素5的idx为" << list.find(5) << endl;
+    cout << "元素80的idx为" << list.find(80) << endl;
+    list.clear();
+    list.print_list();
+    list.insert(19, 0);
+    list.print_list();
+    cout << "*******测试删除*******" << endl;
+    list.del(0);
+    list.del(1);
+    list.push_front(1);
+    list.push_back(4);
+    list.push_front(3);
+    list.insert(5, 3);
+    list.insert(19, 0);
+    list.del(4);
+    list.del(0);
+    list.del(1);
+    cout << "*******测试at*******" << endl;
+    cout << list.at(0) << endl;
+    cout << list.at(1) << endl;
     cout << "*******基础操作集测试完成*******\n" << endl;
+}
 
+void unit_test_advanced_operation(){
+    LinkedList<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(6);
+    list.push_back(14);
+    list.push_back(19);
+    list.push_back(120);
+
+    LinkedList<int> other;
+    other.push_back(-1);
+    other.push_back(0);
+    other.push_back(0);
+    other.push_back(18);
+    
+    cout << "*******测试合并有序链表*******" << endl;
+    LinkedList<int> mergeList = list.merge(other);
+    list.print_list();
+    other.print_list();
+    mergeList.print_list();
+
+    mergeList.reverse();
+    mergeList.print_list();
+    cout << mergeList.hasCircle();
 }
 
 int main(){
     unit_test_base_operation();
+    unit_test_advanced_operation();
     return 0;
 }
