a1217436194f3865ff58ddc207a79e23ea7b5d5e
fang-tech <2819187982@qq.com>
2024-08-08 15:50:19 +0800

    for some test

--- hello world.cpp
+++ hello world.cpp
@@ -2,9 +2,5 @@
 
 int main(){
     std::cout << "hello world" << std::endl;
-    // int a = 1;
-    // int * p1 = &a;
-    // int& p2 = a;
-    // std::cout << sizeof(p1)<< ", " << sizeof(p2);
     return 0;
 }
