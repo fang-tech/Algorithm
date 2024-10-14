b4240b4a1a7203820564e93c9aedb758e9cc7119
fung <2819187982@qq.com>
2024-10-14 22:22:49 +0800

    none

--- Chapter_01_base _data_structure/note/dynamic_list.cpp
+++ Chapter_01_base _data_structure/note/dynamic_list.cpp
@@ -7,10 +7,32 @@ struct node{
 };
 
 int main(){
-    int m, n; scanf(" % d % d", &n, &m);
+    int m, n; scanf(" %d %d", &n, &m);
     node* head;
-    while ((n--) > 1) {
-        for ()
+    head = new node;
+    head->data = 1;
+    node* now = head;
+    for (int i=0; i<n-1; i++) {
+        node* newNode = new node;
+        newNode->data = i+2;
+        newNode->next = nullptr;
+        now->next = newNode;
+        now = newNode;
     }
+    now->next = head;
+    now = head;
+    node* prev = now;
+    while((n--) > 1) {
+        for (int i=1; i<m; i++) {
+            prev = now;
+            now = now->next;
+        }
+        printf("%d ", now->data);
+        prev->next = now->next;
+        delete now;
+        now = prev->next;
+    }
+    printf("%d", now->data);
+    delete now;
     return 0;
 }
