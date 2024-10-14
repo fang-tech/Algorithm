9f897dad7a3cf25b8b9ef5c4bdaacce114a1c903
fung <2819187982@qq.com>
2024-10-14 09:30:39 +0800

    chapter 1 start

---
+++ Chapter_01_base _data_structure/note/dynamic_list.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct node{
+    int data;
+    node* next;
+};
+
+int main(){
+    int m, n; scanf(" % d % d", &n, &m);
+    node* head;
+    while ((n--) > 1) {
+        for ()
+    }
+    return 0;
+}
